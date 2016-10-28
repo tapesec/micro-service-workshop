bbfc9df61f9b71474885d0f8d05517f641aa51c1
DUPOUY <lasalle@MacBook-Pro-de-DUPOUY.local>
2016-10-28 14:58:07 +0200

    doc added on /

--- server.js
+++ server.js
@@ -10,14 +10,21 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-app.user(bodyParser.json({}));
+app.use(bodyParser.json({}));
 
 
 
-app.listen(3010, function() {
-    console.log('server listen 1010');
+app.listen(3000, function() {
+    console.log('server listen 3000');
 });
 
+app.get('/', function(req, res, next) {
+    res.send("<ul>"+
+                "<li>GET  /score => retourne la liste des scores des parties jouées<li>" +
+                "<li>GET  /score/:id => retourne le score de la partie (id)</li>"+
+                "<li>POST /score <pre>{ id: '<id>', score: '<object>' }</pre></li>"+
+            "</ul>");
+})
 
 app.post('/score', function(req, res, next) {
     resultHistory[req.body.id] = req.body;
